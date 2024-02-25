6edb75b79e589bb5eb1c5c6cf50574991907b3bf
ledarca <ledarca@gmail.com>
2024-02-25 14:04:14 -0600

    2 lesson 1-1

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import './App.css'
 
 function App() {
   const todoList = [
-    { id: 1, title: 'Create an empty array and store it in a variable called todoList' },
-    { id: 2, title: 'Create at least 3 objects with the required properties' },
-    { id: 3, title: 'Insert a JavaScript expression in the unordered list' },
-    { id: 4, title: 'Create a level one heading' },
-    { id: 5, title: 'Create an ordered list' },
-    { id: 6, title: 'Assign the todoList array in the JavaScript expression' },
-    { id: 7, title: 'Return a list item (<li>) for each object in the array' },    
+    { id: 1, title: 'Create an empty array and store it in a variable called todoList.' },
+    { id: 2, title: 'Create at least 3 objects with the required properties.' },
+    { id: 3, title: 'Insert a JavaScript expression in the unordered list.' },
+    { id: 4, title: 'Create a level one heading.' },
+    { id: 5, title: 'Create an ordered list.' },
+    { id: 6, title: 'Assign the todoList array in the JavaScript expression.' },
+    { id: 7, title: 'Return a list item (<li>) for each object in the array.' },    
   ];
 
   return (
