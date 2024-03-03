a975d4b0b0a8586a8d826f543985d1761950d4a6
ledarca <ledarca@gmail.com>
2024-03-03 07:21:01 -0600

    change the list, created in leson 1_1 by a component, this second component is called TodoList.jsx inside this component add the form component

---
+++ src/TodoList.jsx
@@ -0,0 +1,29 @@
+import AddTodoForm from './AddTodoForm';
+
+function TodoList() {
+  const todoList = [
+    { id: 1, title: 'Create an empty array and store it in a variable called todoList.' },
+    { id: 2, title: 'Create at least 3 objects with the required properties.' },
+    { id: 3, title: 'Insert a JavaScript expression in the unordered list.' },
+    { id: 4, title: 'Create a level one heading.' },
+    { id: 5, title: 'Create an ordered list.' },
+    { id: 6, title: 'Assign the todoList array in the JavaScript expression.' },
+    { id: 7, title: 'Return a list item (<li>) for each object in the array.' },    
+  ];
+
+  return (
+    <div>
+      <h1>To do list</h1>
+      <AddTodoForm />
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            <strong>{todo.id}</strong>: {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
