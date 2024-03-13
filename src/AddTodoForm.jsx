f1ff08349390e6a46b8f39d8d7acb3a8de678836
ledarca <ledarca@gmail.com>
2024-03-12 20:42:16 -0500

    handle, events, props and useState -  Shows the value you enter visible below the form

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,8 +1,42 @@
-function AddTodoForm() {
+/* function AddTodoForm() 
+{
   return (
     <form>
       <label htmlFor="todoTitle">Title:</label>
       <input type="text" id="todoTitle" />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+export default AddTodoForm; ***1_1 and 1_2 class, start, last part***/
+
+import React, { useState } from 'react';
+
+const AddTodoForm = (props) => {
+
+  const { onAddTodo } = props;
+
+  const [todoTitle, setTodoTitle] = React.useState('');
+
+  // new Function
+  const handleAddTodo = (event) => {
+    // hint
+    event.preventDefault();
+
+    const todoTitle = event.target.title.value;
+
+    console.log(todoTitle);
+
+    event.target.reset();
+
+    // last sentence of the part that says app,invoke the onAddTodo
+    onAddTodo(todoTitle);
+
+  };
+
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input type="text" name="title" />
       <button type="submit">Add</button>
     </form>
   );
