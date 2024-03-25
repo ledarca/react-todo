97b26d5d479ae87fd7fb8851a1fd8949f337724e
ledarca <ledarca@gmail.com>
2024-03-25 05:11:34 -0500

    update the Todo list

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,23 +1,11 @@
-// /src/TodoList.jsx
-import * as React from 'react';
-import TodoListItem from './TodoListItem';
+import React from 'react';
 
-const todoList = [
-    { id: 1, title: 'Prueba1 Create an empty array and store it in a variable called todoList.' },
-    { id: 2, title: 'Create at least 3 objects with the required properties.' },
-    { id: 3, title: 'Insert a JavaScript expression in the unordered list.' },
-    { id: 4, title: 'Create a level one heading.' },
-    { id: 5, title: 'Create an ordered list.' },
-    { id: 6, title: 'Assign the todoList array in the JavaScript expression.' },
-    { id: 7, title: 'Return a list item (<li>) for each object in the array.' },    
-  ];
-//first part
-function TodoList() {
+function TodoList({ todoList }) {
   return (
     <div>
-      <ul> 
+      <ul>
         {todoList.map((todo) => (
-          <TodoListItem key={todo.id} props={todo} />
+          <li key={todo.id}>{todo.title}</li>
         ))}
       </ul>
     </div>
