b4df5e200a55df7514b8a204f207d041bc8e7a2f
ledarca <ledarca@gmail.com>
2024-04-09 20:58:19 -0500

    updated fragment

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,16 @@ function App() {
     setTodoList([...todoList, newTodo]);
   };
 
+  const removeTodo = (id) => {
+    const updatedTodoList = todoList.filter(todo => todo.id !== id); //hint filter
+    setTodoList(updatedTodoList);
+  }
+
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
     </>
   );
 }
