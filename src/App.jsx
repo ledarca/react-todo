e9e895471205b72dfeda34d3a6d34221f30f5954
ledarca <ledarca@gmail.com>
2024-04-20 17:15:02 -0500

    First Async, Second effect useEffect to save the data to localStorage, Create Conditional Loading Indicator

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,33 @@ import React, { useState, useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-function useSemiPersistentState() {
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem('savedTodoList')) || []
-  );
-
-  useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-  }, [todoList]);
-
-  return [todoList, setTodoList];
-}
-
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+  //const Is = JSON.parse(localStorage.getItem('todoList')) ?? [];
+  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('todoList')) ?? []);
+  const [isLoading, setIsLoading] = useState(true);
 
+  // First effect useEffect to load the initial data
+  useEffect(() => {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ data: { todoList } });
+      }, 2000);
+    });
+
+    promise.then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  // Second effect useEffect to save the data to localStorage
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
+
+  // Functions to add and delete tasks
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
@@ -25,19 +37,19 @@ function App() {
   const removeTodo = (id) => {
     const updatedTodoList = todoList.filter(todo => todo.id !== id);
     setTodoList(updatedTodoList);
-  } 
+  };
 
+  // Component rendering
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo}  />
+      {isLoading ? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
     </>
   );
 }
 
 export default App;
-
 
 
 
