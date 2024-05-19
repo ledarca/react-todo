63338b6485e8f8cc288d94c8be3668fa077205b5
ledarca <ledarca@gmail.com>
2024-05-19 11:37:35 -0500

    add game

---
+++ src/NumberGuessingGame.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import GuessControl from './GuessControl';
+import GuessMessage from './GuessMessage';
+import GameOver from './GameOver';
+
+/**
+ * Returns a random integer number from 1-100 inclusive
+ */
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+const MAX_ATTEMPTS = 5;
+
+const NumberGuessingGame = () => {
+  const [numberToGuess, setNumberToGuess] = useState(getRandomNumber());
+  const [numberOfGuesses, setNumberOfGuesses] = useState(0);
+  const [latestGuess, setLatestGuess] = useState(null);
+
+  const handleGuess = (guess) => {
+    setLatestGuess(guess);
+    setNumberOfGuesses((prev) => prev + 1);
+  };
+
+  const handleReset = () => {
+    setNumberToGuess(getRandomNumber());
+    setNumberOfGuesses(0);
+    setLatestGuess(null);
+  };
+
+  const isCorrectGuess = latestGuess === numberToGuess;
+  const isGameOver = isCorrectGuess || numberOfGuesses === MAX_ATTEMPTS;
+
+  return (
+    <div>
+      <h2>I'm thinking of a number from 1 to 100.</h2>
+      <h2>Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?</h2>
+      <GuessControl onGuess={handleGuess} />
+      {isGameOver && <GameOver hasWon={isCorrectGuess} onReset={handleReset} />}
+      {!isGameOver && (
+        <GuessMessage
+          guess={latestGuess}
+          numberToGuess={numberToGuess}
+          numberOfGuesses={numberOfGuesses}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NumberGuessingGame;
