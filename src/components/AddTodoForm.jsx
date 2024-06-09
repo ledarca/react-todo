c9113b23ba7e3f6909360b1da81a0ce5843702ba
ledarca <ledarca@gmail.com>
2024-06-09 05:14:43 -0500

    change folder src to components, add proptypes func

---
+++ src/components/AddTodoForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import InputWithLabel from './InputWithLabel';
+import PropTypes from 'prop-types';
+
+
+function AddTodoForm({ onAddTodo }) {
+  const [todoTitle, setTodoTitle] = useState('');
+
+  const handleTitleChange = (event) => {
+    setTodoTitle(event.target.value);
+  };
+
+  const handleAddTodo = (event) => {
+    event.preventDefault(); // Prevent Default Form Behavior
+    const newTodo = {
+      id: Date.now(), //Temporary Unique Identifier
+      title: todoTitle
+    };
+    onAddTodo(newTodo);
+    setTodoTitle('');
+  };
+
+  return (
+    <form onSubmit={handleAddTodo} className='input-section'>
+      <InputWithLabel id={todoTitle} value={todoTitle} onChange={handleTitleChange} ></InputWithLabel>
+      <button type="submit" id="add-task" className="plus-icon">Add</button>
+    </form>
+  );
+}
+
+AddTodoForm.propTypes = {
+  onAddTodo: PropTypes.func.isRequired,
+};
+
+export default AddTodoForm;
